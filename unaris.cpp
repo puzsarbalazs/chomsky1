d02f940b3c52c89038fc19c502012b1f0ccc5508
puzsarbalazs <61715588+puzsarbalazs@users.noreply.github.com>
2020-03-10 13:17:18 +0100

    Add files via upload

---
+++ unaris.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	int i;
+	cout<< "Add meg a decimális számot!\n";
+	cin>>a;
+	cout<< "Az unáris alakja: ";
+	for (i=1; i<=a; i++)
+	cout<<"1";
+	cout<<"\n";
+	return 0;
+}
+
